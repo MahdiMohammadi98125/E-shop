7023d4eaf64b14690eef62f88c79ed9a71aeecfa
MahdiMohammadi98125 <mahdi.mohammadi012021@gmail.com>
2024-03-16 14:51:52 +0430

    add navbar and footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
+import NavBar from "./components/nav/NavBar";
+import Footer from "./components/footer/Footer";
 
 const poppin = Poppins({ subsets: ["latin"], weight: ["400", "600", "700"] });
 
@@ -16,7 +18,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={poppin.className}>{children}</body>
+      <body className={`${poppin.className} text-slate-700`}>
+        <div className="flex flex-col min-h-screen ">
+          <NavBar />
+          <main className="flex-grow">{children}</main>
+          <Footer />
+        </div>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <div>
       <h1 className="text-5xl font-bold">Hi welcome to this project</h1>
-    </main>
+    </div>
   );
 }
