cdf85e3ee247596436f4c942b4179e4958321dc2
MahdiMohammadi98125 <mahdi.mohammadi012021@gmail.com>
2024-03-16 18:10:00 +0430

    add list products

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,20 @@
+import { products } from "@/utils/products";
 import Container from "./Container";
 import HomeBanner from "./HomeBanner";
+import { truncateText } from "@/utils/truncateText";
 export default function Home() {
   return (
-    <div>
+    <div className="p-8">
       <Container>
-        <div className="p-8">
+        <div>
           <HomeBanner />
+        </div>
+        <div className="grid grid-cols-2 gap-8 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
+          {products.map((product) => (
+            <div key={product.id}>
+              <h1>{truncateText(product.name)}</h1>
+            </div>
+          ))}
         </div>
       </Container>
     </div>
